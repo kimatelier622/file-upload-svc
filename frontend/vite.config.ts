f84118c5d88dcb1a6ff604bd5d5051e14b204ca7
kimatelier622 <lucklytears6@163.com>
2026-08-14 08:29:06 +0800

    fix: issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5179, strictPort: true, proxy: { "/api": { target: "http://localhost:8080", changeOrigin: true } } },
+  server: {
+    port: 5179,
+    strictPort: true,
+    proxy: {
+      "/api": { target: "http://localhost:8080", changeOrigin: true },
+    },
+  },
 });
